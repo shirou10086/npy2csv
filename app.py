bf97a7aae759a132028ddd5371d33add597e46ba
Jinli Duan <jinlid@uci.edu>
2023-11-23 17:19:13 -0500

    下载后直接删除

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import csv
 import os
-from flask import Flask, request, send_file, render_template_string
+from flask import Flask, request, send_file, render_template_string, after_this_request
 
 app = Flask(__name__)
 
@@ -16,10 +16,17 @@ def index():
         with open(csv_filepath, 'w', newline='') as csv_file:
             writer = csv.writer(csv_file)
             writer.writerows(npy_data)
-        return f'''
-        CSV file "{csv_filename}" created successfully! 
-        <a href="/download/{csv_filename}">Download CSV</a>
-        '''
+
+        # 自动触发下载
+        download_url = f"/download/{csv_filename}"
+        return render_template_string('''
+            <script type="text/javascript">
+                window.onload = function() {
+                    window.location.href = "{{ download_url }}";
+                };
+            </script>
+        ''', download_url=download_url)
+
     return '''
     <form method="POST" enctype="multipart/form-data">
         <input type="file" name="file">
@@ -30,6 +37,15 @@ def index():
 @app.route('/download/<filename>', methods=['GET'])
 def download(filename):
     csv_filepath = os.path.join('uploads', filename)
+
+    @after_this_request
+    def remove_file(response):
+        try:
+            os.remove(csv_filepath)
+        except Exception as error:
+            app.logger.error("Error removing or closing downloaded file handle", error)
+        return response
+
     return send_file(csv_filepath, as_attachment=True)
 
 if __name__ == '__main__':
